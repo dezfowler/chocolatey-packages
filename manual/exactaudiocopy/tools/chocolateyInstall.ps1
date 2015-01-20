37811dc653748f0082308e45a7d5f1e121ac57b9
Derek Fowler <dezfowler@gmail.com>
2015-01-20 20:18:49 +0000

    Adding Exact Audio Copy package
    Adding package at 1.0-beta4

---
+++ manual/exactaudiocopy/tools/chocolateyInstall.ps1
@@ -0,0 +1,7 @@
+﻿$packageName = 'exactaudiocopy'
+$installerType = 'exe'
+$url = 'http://www.exactaudiocopy.de/eac-1.0beta4.exe'
+$silentArgs = '/S' 
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage "$packageName" "$installerType" "$silentArgs" "$url" -validExitCodes $validExitCodes
